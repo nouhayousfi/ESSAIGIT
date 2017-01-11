80236f1d3acbb6913b51f5a1ffcc8edf0bb1d80c
nouhayousfi <nouhayousfi09@gmail.com>
2017-01-11 05:34:20 -0600

    commit de model
    les models: class etudiant,class institut et class entreprise

---
+++ PremierEssai/PremierEssai/Model/Entreprise.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PremierEssai.Model
+{
+    class Entreprise
+    {
+        public String NomEntreprise { get; set; }
+        public int Matricule { get; set; }
+        public String AddresseEntreprise { get; set; }
+
+    }
+}
